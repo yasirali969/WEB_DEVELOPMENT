2f4651cded1e9c7e590dac20b7f25807b9d55b4a
Yasir Ali <yasirali.bscssaif25@iba-suk.edu.pk>
2026-07-22 23:18:19 +0500

    Add files via upload

---
+++ JS/String.js
@@ -0,0 +1,35 @@
+let s="Yasir";
+console.log(s);
+
+let y=`I am yasir ali
+student of Sukkur IBA University`;
+console.log(y);
+
+// Upper Lower cases
+let U="yasir";
+console.log(U.toUpperCase())
+
+let l="YASIR";
+console.log(l.toLowerCase())
+
+// length of String
+let len="Yasir is a doctor";
+console.log(len.length);
+
+// Substring
+let sub="I am YasirAli";
+console.log(sub.indexOf("Ali"));
+
+// slice
+let sl="I am student of SukkurIBA University";
+console.log(sl.slice(22,25))
+
+// Replace and Replace ALl
+let rep="Main Campus IBA University is poular among all IBA campuses.";
+//console.log(rep.replace("IBA","LUMS"));
+console.log(rep.replaceAll("IBA","LUMS"));
+
+// Split
+let sp="I am Yasir Ali";
+console.log(sp.split(" "));
+
